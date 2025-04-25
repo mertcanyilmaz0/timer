096189f7d66ed1fd4b63ef87b5c62b6b6237e6d2
merd <mertcanyilmaz5420@gmail.com>
2025-04-25 21:14:28 +0300

    create settings

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function endTimer() {
   console.log("Zamanlayıcı durduruldu");
 }
 function startTimer() {
-  clearInterval(countdown); 
+  clearInterval(countdown);
 
   const minutes = parseInt(document.getElementById("minuteInput").value);
   if (isNaN(minutes) || minutes <= 0) {
@@ -30,7 +30,7 @@ function startTimer() {
 
     if (totalSeconds <= 0) {
       clearInterval(countdown);
-      sendNotification(); 
+      sendNotification();
     }
 
     totalSeconds--;
@@ -49,46 +49,52 @@ function sendNotification() {
   }
 }
 
-window.onload = function() {
+window.onload = function () {
   requestNotificationPermission();
 };
 
 let destroyPoint = 1
-function destroyed(){
+function destroyed() {
   const minutes = parseInt(document.getElementById("minuteInput").value);
   const container = document.getElementById("container")
   const timer = document.getElementById("timer")
-  const exit = document.querySelector("#exit")
-  if (isNaN(minutes) || minutes <= 0){
+  const exit = document.getElementById("exit")
+  const work = document.getElementById("work")
+  const settings = document.getElementById("settings")
+  if (isNaN(minutes) || minutes <= 0) {
     alert("Lütfen geçerli bir dakika değeri girin.");
     return;
   } else {
-    if (destroyPoint === 0){
+    if (destroyPoint === 0) {
       container.style.display = "none";
-      timer.style.display = "flex"
+      //timer.style.display = "flex"
       timer.style.justifyContent = "center";
-      timer.style.alignItems = "center"; 
-      timer.style.height = "calc(100vh - 10rem)";
+      settings.style.display = "none"
+      timer.style.alignItems = "center";
+      work.style.height = "calc(100vh - 4rem)";
       timer.style.marginTop = "0rem";
       exit.style.display = "block";
-    } if (destroyPoint === 1){{
+    } if (destroyPoint === 1) {
+      {
         container.style.display = "block";
-        timer.style.height = "";
+        settings.style.display = "block"
+        work.style.height = "";
         exit.style.display = "none";
         endTimer()
+      }
     }
   }
-  }};
-document.querySelector("#timer").addEventListener("click", function(){
-  if (destroyPoint === 0){
+};
+document.querySelector("#timer").addEventListener("click", function () {
+  if (destroyPoint === 0) {
     console.log("0");
     destroyPoint = 1;
     destroyed()
   }
 })
-document.querySelector("#btn").addEventListener("click", function(){
+document.querySelector("#btn").addEventListener("click", function () {
   startTimer()
-  if (destroyPoint === 1){
+  if (destroyPoint === 1) {
     destroyPoint = 0;
     destroyed()
     console.log("1");
@@ -102,18 +108,61 @@ customBtn.addEventListener('click', () => {
   input.click(); // Tıklanınca gizli input'u tetikleme
 });
 
-input.addEventListener('change', function(event) {
+input.addEventListener('change', function (event) {
   const file = event.target.files[0];
-  
+  const navbar = document.getElementById("navbar");
+
   if (file && file.type.startsWith('image/')) {
     const reader = new FileReader();
-    
-    reader.onload = function(e) {
+
+    reader.onload = function (e) {
       document.body.style.backgroundImage = `url('${e.target.result}')`;
+      navbar.style.backgroundImage = `url('${e.target.result}')`
     };
-    
+
     reader.readAsDataURL(file);
   } else {
     alert("Lütfen bir resim dosyası seçin.");
   }
 });
+
+const soz = document.getElementById("ozluSoz");
+
+soz.addEventListener("change", function(){
+  if (soz.checked) {
+    const text = document.getElementById("motivation");
+    console.log("Kutu işaretli.");
+    text.style.display = "inline"
+  } else {
+    const text = document.getElementById("motivation");
+    console.log("Kutu işaretli değil.");
+    text.style.display = "none";
+  };
+});
+
+let navbarPoint = 1;
+const carp = document.getElementById("carpi");
+const settings = document.getElementById("settings");
+console.log("navbar point 1");
+settings.addEventListener("click", function(){
+if(navbarPoint === 1){
+  const navbar = document.getElementById("navbar");
+  navbar.style.display = "block"
+  navbar.style.position = "fixed"
+  navbar.style.left = "0"
+  navbar.style.top = "0"
+  navbarPoint = 0
+  console.log("navbar point 0");
+};
+});
+carp.addEventListener("click", function(){
+  if(navbarPoint === 0){
+      const navbar = document.getElementById("navbar");
+      navbar.style.display = "none"
+      navbar.style.position = ""
+      navbar.style.left = ""
+      navbar.style.top = ""
+      navbarPoint = 1
+      console.log("navbar point 1");
+  }
+})
